5f12986f5445db6f1f3568f6d1e47fb5a7965fcc
EC2 Default User <ec2-user@ip-172-31-25-211.ap-south-1.compute.internal>
2022-08-09 10:14:52 +0000

    update XML properties from params, gitignore added

--- utils.py
+++ utils.py
@@ -55,9 +55,28 @@ def read_yolo(path):
             "height": float(data[4])}
 
 
-def create_xml():
-    print(xmltodict.unparse(XML_STRUCTURE, pretty= True))
+demo_xml = {"folder", "filename", "database", "path", "width", "height", "depth", "segmented", "label", "pose", "truncated", "difficult", "occluded", "x_top_left", "y_top_left", "x_bottom_right", "y_bottom_right"}
 
+def create_xml(file_path, updated_details, XML_STRUCTURE=XML_STRUCTURE):
+    XML_STRUCTURE['annotation']["folder"] = updated_details.get("folder", None)
+    XML_STRUCTURE['annotation']["filename"] = updated_details.get("filename", None)
+    XML_STRUCTURE['annotation']["path"] = updated_details.get("path", None)
+    XML_STRUCTURE['annotation']["source"]["database"] = updated_details.get("database", None)
+    XML_STRUCTURE['annotation']["size"]["width"] = updated_details["width"]
+    XML_STRUCTURE['annotation']["size"]["height"] = updated_details["height"]
+    XML_STRUCTURE['annotation']["size"]["depth"] = updated_details["depth"]
+    XML_STRUCTURE['annotation']["segmented"] = updated_details.get("segmented", None)
+    XML_STRUCTURE['annotation']["object"]["name"] = updated_details["label"]
+    XML_STRUCTURE['annotation']["object"]["pose"] = updated_details.get("pose", None)
+    XML_STRUCTURE['annotation']["object"]["trucated"] = updated_details.get("truncated", None)
+    XML_STRUCTURE['annotation']["object"]["difficult"] = updated_details("difficult", None)
+    XML_STRUCTURE['annotation']["object"]["bndbox"]["xmin"] = updated_details["x_top_left"]
+    XML_STRUCTURE['annotation']["object"]["bndbox"]["xmax"] = updated_details["x_bottom_right"]
+    XML_STRUCTURE['annotation']["object"]["bndbox"]["ymin"] = updated_details["y_top_left"]
+    XML_STRUCTURE['annotation']["object"]["bndbox"]["ymax"] = updated_details["y_bottom_right"]
+    
+    with open(file_path, "w") as f:
+        f.write(xmltodict.unparse(XML_STRUCTURE, pretty= True))
 
 
 if __name__ == "__main__":
